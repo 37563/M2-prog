87ff721a577368e4e7acc946490375bfbd7f724d
Jay <37563@ma-web.nl>
2023-11-21 12:01:28 +0100

    edit

---
+++ JS/01A/app.js
@@ -0,0 +1,17 @@
+class app
+{
+    runApllication()
+    {
+        console/log("hello world!");
+    }
+}
+
+let app = new app();
+app.runApllication();
+
+let appNaam = "Test"
+let Versie = 0.1
+let makeOrigin = 21/11/2023 + "ATM" + 11.56
+let Author = "ZV"
+let copyright = "ZV inc."
+let darkmode = false
